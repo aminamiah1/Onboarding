f43a248e3b73b3e547876a51afe4b69c38159684
c21040245 <milliganEC@cardiff.ac.uk>
2022-11-21 14:08:41 +0000

    Added gradle files

---
+++ src/main/java/com/example/ase2022y203/Ase2022Y203Application.java
@@ -0,0 +1,13 @@
+package com.example.ase2022y203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ase2022Y203Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ase2022Y203Application.class, args);
+    }
+
+}
