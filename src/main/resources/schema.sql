1a23e882a09d537301d90734f75078440c4a5e0f
c21040245 <milliganEC@cardiff.ac.uk>
2022-11-22 10:33:54 +0000

    Re-defined Schema.sql According to yesterday's planning session

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,29 +2,86 @@ SET MODE MYSQL;
 SET IGNORECASE = TRUE;
 
 -- -----------------------------------------------------
--- TABLE `CHARITY`
+-- SCHEMA `My_DB`
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS 'My_DB';
+USE 'My_DB';
+
+DROP TABLE IF EXISTS Candidates;
+DROP TABLE IF EXISTS Personal_Information;
+DROP TABLE IF EXISTS Candidate_References;
+DROP TABLE IF EXISTS Vetting_Officers;
+DROP TABLE IF EXISTS Applications;
+
+    -- -----------------------------------------------------
+-- TABLE `Candidates`
 -- -----------------------------------------------------
 
-DROP TABLE IF EXISTS CANDIDATES;
-DROP TABLE IF EXISTS PERSONAL_INFORMATION;
-
-
-CREATE TABLE IF NOT EXISTS CANDIDATES
+CREATE TABLE IF NOT EXISTS Candidates
 (
-    ID              INTEGER AUTO_INCREMENT NOT NULL,
-    EMAIL           VARCHAR(200)          NOT NULL UNIQUE,
-    FIRST_NAME           VARCHAR(200)            NOT NULL,
-    SURNAME         VARCHAR(200)         NOT NULL,
-    COMPANY_NAME   VARCHAR(5)             NOT NULL,
+    ID            INTEGER AUTO_INCREMENT NOT NULL,
+    First_Name    VARCHAR(200),
+    Surname       VARCHAR(200),
+    Email         VARCHAR(200) NOT NULL UNIQUE,
+    Password      VARCHAR(200) NOT NULL UNIQUE,
+    Company_Name  VARCHAR(200),
     PRIMARY KEY (`ID`)
 )
     ENGINE = INNODB;
 
-CREATE TABLE IF NOT EXISTS CANDIDATE_INFORMATION (
+-- -----------------------------------------------------
+-- TABLE `Personal_Information`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Personal_Information (
     ID INTEGER AUTO_INCREMENT NOT NULL,
-    CID INTEGER NOT NULL,
-    NI INTEGER NOT NULL,
+    C_ID INTEGER NOT NULL,
+    National_Insurance INTEGER,
+    Ethnicity VARCHAR(200),
+    Gender VARCHAR(200),
+    Age INTEGER,
+    Sexuality VARCHAR(200),
+    PRIMARY KEY (`ID`)
 )
     ENGINE = INNODB;
 
+-- -----------------------------------------------------
+-- TABLE `Vetting_Officers`
+-- -----------------------------------------------------
 
+CREATE TABLE IF NOT EXISTS Vetting_Officers (
+    ID INTEGER AUTO_INCREMENT NOT NULL,
+    First_Name VARCHAR(200),
+    Surname VARCHAR(200),
+    Email VARCHAR(200) NOT NULL UNIQUE,
+    Password VARCHAR(200) NOT NULL UNIQUE,
+    App_ID INTEGER NOT NULL,
+    PRIMARY KEY('ID')
+)
+    ENGINE = INNODB;
+
+-- -----------------------------------------------------
+-- TABLE `Candidate_References`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Candidate_References (
+    ID INTEGER NOT NULL,
+    C_ID INTEGER NOT NULL,
+    Referee VARCHAR(200),
+    Referee_Number INTEGER,
+    PRIMARY KEY('ID')
+)
+    ENGINE = INNODB;
+
+-- -----------------------------------------------------
+-- TABLE `Applications`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Applications (
+    ID INTEGER NOT NULL,
+    App_Status VARCHAR(200),
+    C_ID INTEGER NOT NULL,
+    R_ID INTEGER NOT NULL,
+    PRIMARY KEY('ID')
+)
+    ENGINE = INNODB;
