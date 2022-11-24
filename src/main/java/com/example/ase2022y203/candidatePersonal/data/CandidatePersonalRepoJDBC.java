9e6b7fe334eb4dffd58d3ed8bcc00c3ae122b232
c21040245 <milliganEC@cardiff.ac.uk>
2022-11-24 15:14:30 +0000

    Added DTO and repositories for accessing candidates personal information.

--- src/main/java/com/example/ase2022y203/candidatePersonal/data/CandidatePersonalRepoJDBC.java
+++ src/main/java/com/example/ase2022y203/candidatePersonal/data/CandidatePersonalRepoJDBC.java
@@ -3,9 +3,9 @@ package com.example.ase2022y203.candidatePersonal.data;
 import com.example.ase2022y203.candidatePersonal.domain.CandidatePersonal;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface CandidatePersonalRepository{
-        Optional<CandidatePersonal> getCandidatePersonalInfoByCID(Integer cid);
+public interface CandidatePersonalRepoJDBC extends CrudRepository<CandidatePersonal, Integer> {
+    Iterable<CandidatePersonal> findAll();
+    Optional<CandidatePersonal> findByC_id(Integer cid);
 }
--- src/main/java/com/example/ase2022y203/candidatePersonal/data/CandidatePersonalRepositoryImpl.java
+++ src/main/java/com/example/ase2022y203/candidatePersonal/data/CandidatePersonalRepositoryImpl.java
@@ -1,4 +1,20 @@
 package com.example.ase2022y203.candidatePersonal.data;
 
-public class CandidatePersonalRepositoryImpl {
+import com.example.ase2022y203.candidatePersonal.domain.CandidatePersonal;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class CandidatePersonalRepositoryImpl implements CandidatePersonalRepository {
+
+    private CandidatePersonalRepoJDBC candidatePersonalJdbcRepo;
+    public CandidatePersonalRepositoryImpl(CandidatePersonalRepoJDBC aRepo){
+        candidatePersonalJdbcRepo = aRepo;
+    }
+    @Override
+    public Optional<CandidatePersonal> getCandidatePersonalInfoByCID(Integer cid) {
+        return candidatePersonalJdbcRepo.findByC_id(cid);
+    }
+
 }
