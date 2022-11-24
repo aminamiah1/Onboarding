53835da81cf8bc158d429ec2a5d70c4ba01801ea
c21040245 <milliganEC@cardiff.ac.uk>
2022-11-24 13:30:41 +0000

    Added spring data jdbc to gradle.

--- src/main/java/com/example/ase2022y203/candidatePersonal/domain/CandidatePersonal.java
+++ src/main/java/com/example/ase2022y203/candidatePersonal/domain/CandidatePersonal.java
@@ -1,4 +1,20 @@
 package com.example.ase2022y203.candidatePersonal.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@Table
 public class CandidatePersonal {
+    private Integer id;
+    @MappedCollection(idColumn="C_ID")
+    private Integer c_id;
+    private String national_insurance;
+    private String ethnicity;
+    private String gender;
+    private String age;
+    private String sexuality;
 }
