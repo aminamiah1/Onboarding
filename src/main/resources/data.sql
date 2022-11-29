8f87b2e6e20ba99039de860be8a14067dbf5daea
c21040245 <milliganEC@cardiff.ac.uk>
2022-11-29 11:35:35 +0000

    Using Bcrypt passwords for each candidate

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,10 +7,10 @@ INSERT INTO candidates(ID, First_Name, Surname, Email, Password, Company_Name)
 VALUES(1, 'Kerry', 'Malik', 'KMalik@gmail.com', '$2a$12$1bYa79yXePS4DdQiOtW8quDj2HMlzClpcHnAJdkRDlGe/vIYxvBB2', 'Admiral');
 
 INSERT INTO candidates(ID, First_Name, Surname, Email, Password, Company_Name)
-VALUES(2, 'Terry', 'Houston', 'TH@gmail.com', 'IamTerry@123', 'Farmland');
+VALUES(2, 'Terry', 'Houston', 'TH@gmail.com', '$2a$12$KONcg/S5POa162hjUWH0I.p6SRfCGIy.7LCl8mDJC803rqsDTUcUS', 'Farmland');
 
 INSERT INTO candidates(ID, First_Name, Surname, Email, Password, Company_Name)
-VALUES(3, 'Molly', 'Jacobsen', 'MJ@gmail.com', 'IamMolly@123', 'JCB');
+VALUES(3, 'Molly', 'Jacobsen', 'MJ@gmail.com', '$2a$12$NOK89HvHvh3ou.XBvE7KreC74YFy8wKl35WXE68OMzEHXBnuQ.EaO', 'JCB');
 
 INSERT INTO personal_information(CID, national_insurance, ethnicity, gender, age, sexuality)
 VALUES(1, 'QQ123456A', 'White', 'Female', '48', 'heterosexual');
