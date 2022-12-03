20f5605e7a39f942a9e9314aeb87316f48dcb995
c21040245 <milliganEC@cardiff.ac.uk>
2022-12-03 16:24:29 +0000

    Vetting officers and master admins can view all candidates. Will rip out candidate/all-candidates path after client meeting as it is insecure and only vetting officers and master admins should be able to view all candidates' information. Also allowed the navbar to be changed depending on who has logged in to view the page.

--- src/main/java/com/example/ase2022y203/vettingOfficers/web/VettingOfficerController.java
+++ src/main/java/com/example/ase2022y203/vettingOfficers/web/VettingOfficerController.java
@@ -1,6 +1,10 @@
 package com.example.ase2022y203.vettingOfficers.web;
 
 
+import com.example.ase2022y203.candidate.service.CandidateService;
+import com.example.ase2022y203.candidate.service.messages.CandidateListRequest;
+import com.example.ase2022y203.candidate.service.messages.CandidateListResponse;
+import com.example.ase2022y203.candidatePersonal.service.CandidatePersonalService;
 import com.example.ase2022y203.vettingOfficers.service.VettingOfficersDTO;
 import com.example.ase2022y203.vettingOfficers.service.VettingOfficersService;
 import org.springframework.security.core.Authentication;
@@ -11,15 +15,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("officer")
 public class VettingOfficerController {
+    private final CandidateService candidateService;
 
     private final VettingOfficersService vettingOfficersService;
 
-    public VettingOfficerController(VettingOfficersService vos) {
+    public VettingOfficerController(CandidateService candidateService, VettingOfficersService vos) {
+        this.candidateService = candidateService;
         this.vettingOfficersService = vos;
     }
 
@@ -34,6 +41,22 @@ public class VettingOfficerController {
         if(vettingOfficer.isPresent()){
             model.addAttribute("officer", vettingOfficer.get());
             var mv = new ModelAndView("officer/officer-profile", model.asMap());
+            return mv;
+        } else {
+            return new ModelAndView("redirect:/404");
+        }
+    }
+
+    @GetMapping("view-candidates")
+    public ModelAndView getAllCandidates(Model model, HttpServletRequest request) {
+        if (request.isUserInRole("ROLE_ADMIN") | request.isUserInRole("ROLE_OFFICER")) {
+            CandidateListRequest candidateListRequest = CandidateListRequest
+                    .of()
+                    .build();
+            CandidateListResponse candidateListResponse = candidateService.getCandidates(candidateListRequest);
+            System.out.println(candidateListResponse.getCandidates());
+            model.addAttribute("candidates", candidateListResponse.getCandidates());
+            var mv = new ModelAndView("candidate/all-candidates", model.asMap());
             return mv;
         } else {
             return new ModelAndView("redirect:/404");
