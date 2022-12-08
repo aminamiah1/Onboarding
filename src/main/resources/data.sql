aa6e0cc76d1134aee97107a59eb6dfe0611eac4c
Amina Miah <miaha9@cardiff.ac.uk>
2022-12-08 20:35:34 +0000

    added tests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,6 +24,15 @@ VALUES(3);
 INSERT INTO vetting_officers(First_name, surname, email, password)
 VALUES('Sarah', 'Radcliffe', 'SR@gmail.com', '$2a$12$u8rpeK5sRXjUuGxASxtHEeLIAqXtCLrosyjAh2sNHoo93ovm.8qZS');
 
+INSERT INTO vetting_officers(First_name, surname, email, password)
+VALUES('Emily', 'Clarke', 'EC@gmail.com','EmilyClarke212@');
+
+INSERT INTO vetting_officers(First_name, surname, email, password)
+VALUES('Khalid', 'Rahman', 'KR@gmail.com','MyPasswordIs1@');
+
+INSERT INTO vetting_officers(First_name, surname, email, password)
+VALUES('Amina', 'Miah', 'AminaMiah@gmail.com','Amina112@');
+
 INSERT INTO candidate_references(ID, CID, Referee_Name, Referee_Phone_Number)
 VALUES(1, 1, 'Angela Mooring', '+447975777666');
 
