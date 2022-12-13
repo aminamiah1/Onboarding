20f26076746daee1310e00ea77a803205603da72
c21040245 <milliganEC@cardiff.ac.uk>
2022-12-13 11:14:36 +0000

    Candidate can upload passport as well, both files are now named to the appropriate file name for example PP_C1 on submission of a file. Set to only allow jpeg for files as well.

--- src/main/java/com/example/ase2022y203/candidateDocuments/web/FileUploadController.java
+++ src/main/java/com/example/ase2022y203/candidateDocuments/web/FileUploadController.java
@@ -1,11 +1,15 @@
 package com.example.ase2022y203.candidateDocuments.web;
 
+import com.example.ase2022y203.candidate.service.CandidateDTO;
+import com.example.ase2022y203.candidate.service.CandidateService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +20,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 import static java.nio.file.Files.probeContentType;
 import static java.nio.file.Files.write;
@@ -24,13 +29,20 @@ import static java.nio.file.Files.write;
 @Controller
 @SessionAttributes({"passportFile, IDFile"})
 public class FileUploadController {
+
+    private final CandidateService candidateService;
+
+    public FileUploadController(CandidateService candidateService) {
+        this.candidateService = candidateService;
+    }
+
     //Directory where the candidate documents are stored
     @Value("${candidate.documents.directory}")
     private String candidateDocumentPathSuffix;
 
     @GetMapping("candidateDocuments/{fileName}")
     public ResponseEntity<Resource> downloadFile(@PathVariable String fileName){
-        String homePath = "/resources";
+        String homePath = System.getProperty("user.dir");
         String candidateDocumentPath = homePath.concat(candidateDocumentPathSuffix);
         Path path = Paths.get(candidateDocumentPath + fileName);
 
@@ -49,6 +61,11 @@ public class FileUploadController {
 
     @PostMapping("/uploadID")
     public String saveID(@RequestParam("idFile") MultipartFile idFile, Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipleEmail = authentication.getName();
+
+        Optional<CandidateDTO> candidate = candidateService.getCandidateByEmail(currentPrincipleEmail);
+
         String homePath = System.getProperty("user.dir");
         String candidateDocumentPath = homePath.concat(candidateDocumentPathSuffix);
         File documentDirectory = new File(candidateDocumentPath);
@@ -64,11 +81,45 @@ public class FileUploadController {
             throw new RuntimeException(e);
         }
 
-        Path path = Paths.get(candidateDocumentPath + idFile.getOriginalFilename());
+        Path path = Paths.get(candidateDocumentPath + "ID_" + "C" + candidate.get().getId().toString() + ".jpg");
         System.out.println("Writing file: " + candidateDocumentPath + idFile.getOriginalFilename());
         try{
             write(path, bytes);
-            model.addAttribute("lastFile", idFile.getOriginalFilename());
+            model.addAttribute("lastIDFile", idFile.getOriginalFilename());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "redirect:/candidate/document-portal";
+    }
+
+    @PostMapping("/uploadPassport")
+    public String savePassport(@RequestParam("passportFile") MultipartFile passportFile, Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipleEmail = authentication.getName();
+
+        Optional<CandidateDTO> candidate = candidateService.getCandidateByEmail(currentPrincipleEmail);
+
+        String homePath = System.getProperty("user.dir");
+        String candidateDocumentPath = homePath.concat(candidateDocumentPathSuffix);
+        File documentDirectory = new File(candidateDocumentPath);
+
+        if(!documentDirectory.exists()){
+            documentDirectory.mkdir();
+        }
+
+        byte[] bytes = new byte[0];
+        try{
+            bytes = passportFile.getBytes();
+        } catch(IOException e){
+            throw new RuntimeException(e);
+        }
+
+        Path path = Paths.get(candidateDocumentPath + "PP_" + "C" + candidate.get().getId().toString() + ".jpg");
+        System.out.println("Writing file: " + candidateDocumentPath + passportFile.getOriginalFilename());
+        try{
+            write(path, bytes);
+            model.addAttribute("lastPassportFile", passportFile.getOriginalFilename());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
